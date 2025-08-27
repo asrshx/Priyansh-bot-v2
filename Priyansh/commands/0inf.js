2fdb2d5c11926458ff9c211824d03361874bc6ac
𝐀𝐑𝐒𝐇`𝐗´ 😈 <149147034+asrshx@users.noreply.github.com>
2025-08-28 00:27:14 +0530

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -2,9 +2,9 @@ module.exports.config = {
 	name: "inf",
 	version: "1.0.1", 
 	hasPermssion: 0,
-	credits: "Arun Kumar", //don't change the credits please
+	credits: "Joshua Sy", //don't change the credits please
 	description: "Admin and Bot info.",
-	commandCategory: "info",
+	commandCategory: "...",
 	cooldowns: 1,
 	dependencies: 
 	{
@@ -22,30 +22,25 @@ const time = process.uptime(),
 		minutes = Math.floor((time % (60 * 60)) / 60),
 		seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Kolkata").format("『D/MM/YYYY』 【HH:mm:ss】");
-var link =                                     
-["https://i.imgur.com/kNmsmiT.gif","https://i.imgur.com/rNIC7Bq.jpeg"];
-var callback = () => api.sendMessage({body:`🆃︎🅷︎🅴︎ 🅾︎🆆︎🅽︎🅴︎🆁︎ 🅸︎🅽︎🅵︎🅾︎
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
+var link = ["https://i.imgur.com/iit7sl4.jpeg", "https://i.imgur.com/fGLKxjH.gif", "https://i.imgur.com/lmxK9dq.jpeg", "https://i.imgur.com/dP8RlXz.jpeg", "https://i.imgur.com/NILOrbu.jpeg", "https://i.imgur.com/YSMgWv7.jpeg", "https://i.imgur.com/NDK8oJZ.jpeg", "https://i.imgur.com/121yKVj.jpeg",];
+var callback = () => api.sendMessage({body:`✦𝗔𝗗𝗠𝗜𝗠 𝗔𝗡𝗗 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡✦
 
+⁂BoT NaMe ⊂◉‿◉: ${global.config.BOTNAME}
 
-☄️𝗕𝗢𝗧 𝗡𝗔𝗠𝗘☄️ ⚔ ${global.config.BOTNAME} ⚔
+✡BoT Prefix ◉‿◉: ${global.config.PREFIX}
 
-🔥𝗢𝗪𝗡𝗘𝗥 🔥☞︎︎︎ KABEER ☜︎︎︎✰ \n\n
-🙈🄾🅆🄽🄴🅁 🄲🄾🄽🅃🄰🄲🅃 🄻🄸🄽🄺🅂🙈➪ \n\n  𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 🧨https://www.facebook.com/61577780325830
-  \n 
-✅𝗜𝗡𝗦𝗧𝗔𝗚𝗥𝗔𝗠 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘👉 @Kabeer__Xwd \n\n  ====𝗧𝗼 𝗹𝗲𝗮𝗿𝗻 𝗛𝗼𝘄 𝘁𝗼 𝗖𝗿𝗲𝗮𝘁𝗲 𝗔 𝗯𝗼𝘁 === 𝗩𝗶𝘀𝗶𝘁 𝗔𝗻𝗱 𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 𝗧𝗼 𝗠𝘆 𝗖𝗵𝗮𝗻𝗻𝗲𝗹✅ 🗡 https:𝐎𝐰𝐧𝐞𝐫 𝐒𝐞 𝐋𝐞𝐥𝐞 𝐊𝐞 𝐒𝐮𝐛 𝐌𝐞 𝐇𝐢 𝐃𝐮 𝐀𝐛
-✧══════•❁❀❁•══════✧
+༻𝐎𝐖𝐍𝐄𝐑:- ☞꧁𓊈𒆜HENRY XWD𒆜𓊉꧂☜ contact my owner for any issue.😁
 
-🌸𝗕𝗼𝘁 𝗣𝗿𝗲𝗳𝗶𝘅🌸☞︎︎︎☜︎︎︎✰ ${global.config.PREFIX}
+༒𝐇𝐢𝐬 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐢𝐝༒:- ☞ https://www.facebook.com/Henry.inxide
+ 
+➟UPTIME☆
 
-🥳UPTIME🥳
+✬Today Is: ${juswa} 
 
-🌪️Today is🌪️ ☞︎︎︎☜︎︎︎✰ ${juswa} 
+➳BoT Is Running ${hours}:${minutes}:${seconds}.
 
-⚡Bot is running⚡ ${hours}:${minutes}:${seconds}.
-
-✅Thanks for using My Bot ❤ ${global.config.BOTNAME} 🖤
-
-`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/juswa.jpg")).on("close",() => callback());
+✫Thanks For Using ${global.config.BOTNAME} BoT!`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
    };
+				     
